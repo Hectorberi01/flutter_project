4ff00c9652cd8669a0ff2512b090f480c3f22769
Hector <hector.adjakpa@gmail.com>
2026-03-31 23:53:12 +0200

    update

--- mobile_app/lib/core/api/constants.dart
+++ mobile_app/lib/core/api/constants.dart
@@ -6,7 +6,7 @@ class AppConstants {
   // iOS simulator     → localhost ou 127.0.0.1
   // Appareil physique → IP locale de ta machine (ex: 192.168.1.x)
   // static const String baseUrl = 'http://10.0.2.2:3000/api';
-  static const String baseUrl = 'https://5fc5-46-193-57-242.ngrok-free.app/api';
+  static const String baseUrl = 'https://0f23-77-132-48-197.ngrok-free.app/api';
 
   // ── Storage keys ─────────────────────────────────────────────────────────
   static const String tokenKey = 'auth_token';
